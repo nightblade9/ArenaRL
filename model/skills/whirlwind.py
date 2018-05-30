42e5d9d26b42de956e4d119c6bfd03214da317b4
nightblade9 <nightbladecodes@gmail.com>
2018-05-30 13:08:03 -0400

    Whirlwind does lightning damage.

--- model/skills/whirlwind.py
+++ model/skills/whirlwind.py
@@ -1,11 +1,14 @@
 from game import Game
-
+from elements import Element
 
 class Whirlwind:
-    # modified copypasta from cast_fireball
-    # TODO: DRY?
     @staticmethod
     def process(player, radius, area_map):
         for obj in area_map.entities:
-            if obj.distance(player.x, player.y) <= radius and Game.instance.fighter_system.has(obj) and Game.instance.fighter_system.get(obj).hostile:
-                Game.instance.fighter_system.get(player).attack(obj)
+            if obj.distance(player.x, player.y) <= radius and Game.instance.fighter_system.has(obj) \
+            and Game.instance.fighter_system.get(obj).hostile:
+                player_fighter = Game.instance.fighter_system.get(player)
+                target_fighter = Game.instance.fighter_system.get(obj)
+                # attacks and applies elemental damage
+                damage = player_fighter.calculate_damage(1, target_fighter)
+                target_fighter.take_damage(damage, Element.LIGHTNING)
